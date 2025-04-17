4413aeb86fdd867d3dcb1f57965a052a9d688834
Pius Aaron <piuschbz@gmail.com>
2025-04-17 07:06:10 +0100

    Login and registration endpoints

--- src/modules/auth/controllers/auth.controller.ts
+++ src/modules/auth/controllers/auth.controller.ts
@@ -1,21 +1,45 @@
-import { Controller, Post, UseGuards, Request, Body } from '@nestjs/common';
+import { Request } from 'express';
+import { Controller, Post,
+  UseGuards, Request as NestRequest,
+  Body, HttpException } from '@nestjs/common';
 import { AuthService } from '../services/auth.service';
 import { AuthGuard } from '@nestjs/passport';
 import { RegisterAdminDto } from '../dtos/register-admin.dto';
+import { LoginDto } from '../dtos/login.dto';
 
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-  @UseGuards(AuthGuard('local'))
-  @Post('login')
-  async login(@Request() req: any) {
-    return this.authService.login(req.user);
+
+  @Post('/login')
+  async login(@Body() dto: LoginDto) {
+    const user = await this.authService.validateUser(dto.email, dto.password);
+    if (!user) {
+      throw new HttpException('Invalid credentials', 401);
+    }
+    return this.authService.login(user);
   }
 
+
+  // Only super Admin can register an admin
+  @UseGuards(AuthGuard('jwt'))
   @Post('register-admin')
-  async registerAdmin(@Body() dto: RegisterAdminDto) {
+  async registerAdmin(@NestRequest() req: Request, @Body() dto: RegisterAdminDto) {
+    const user = req.user as { sub: string };
+  
+    const isSuperAdmin = await this.authService.isSuperAdmin(user.sub);
+    if (!isSuperAdmin) {
+      throw new HttpException('Unauthorized', 401);
+    }
+  
     return this.authService.registerAdmin(dto);
+  }
+
+  @Post('register-tenant-admin')
+  async registerTenantAdmin(@Body() dto: RegisterAdminDto) {
+    const user = await this.authService.registerAdmin(dto);
+    return user;
   }
 }
